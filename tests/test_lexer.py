5216c73ed9462c1df2d605815652e753688fee19
leogout <leogout@gmail.com>
2024-10-06 23:01:57 +0200

    WIP

--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -57,14 +57,50 @@ def test_var_assignment(lexer):
     
     assert tokens == expected_tokens
 
-def test_keypress_command(lexer):
-    code = '\n'.join(Lexer.COMMANDS) + '\n'
+def test_all_keypress_commands(lexer):
+    code = '\n'.join(Lexer.COMMANDS)
     tokens = list(lexer.tokenize(code))
-
-    expected_tokens = [
-        Token(TokenType.KEYPRESS, command, i + 1, 0) for i, command in enumerate(Lexer.COMMANDS)
-    ]
+    
+    expected_tokens = []
+    for i, command in enumerate(Lexer.COMMANDS):
+        expected_tokens.append(Token(TokenType.KEYPRESS, command, i + 1, 0))
+        expected_tokens.append(Token(TokenType.NEWLINE, '\n', i + 1, len(command)))
+    
     expected_tokens.append(Token(TokenType.EOF, '', len(Lexer.COMMANDS) + 1, 0))
 
     assert tokens == expected_tokens
-    
+
+def test_all_keywords_tokens(lexer):
+    code = """
+    STRING Hello, World!
+    VAR $x = 1 + 2 * 3 / 4 - 5 & 6 | 7 << 8 >> 9
+    VAR $y = 1 == 2 != 3 > 4 < 5 >= 6 <= 7 && 8 || 9
+    DELAY 10
+    """
+    tokens = list(lexer.tokenize(code))
+    expected_tokens = [
+        Token(TokenType.PRINTSTRING, 'STRING', 1, 0),
+        Token(TokenType.STRING, 'Hello, World!', 1, 8),
+        Token(TokenType.VAR, 'VAR', 2, 0),
+        Token(TokenType.ID, '$x', 2, 4),
+        Token(TokenType.ASSIGN, '=', 2, 7),
+        Token(TokenType.NUMBER, '1', 2, 9),
+        Token(TokenType.OP, '+', 2, 11),
+        Token(TokenType.NUMBER, '2', 2, 13),
+        Token(TokenType.OP, '*', 2, 15),
+        Token(TokenType.NUMBER, '3', 2, 17),
+        Token(TokenType.OP, '/', 2, 19),
+        Token(TokenType.NUMBER, '4', 2, 21),
+        Token(TokenType.OP, '-', 2, 23),
+        Token(TokenType.NUMBER, '5', 2, 25),
+        Token(TokenType.OP, '&', 2, 27),
+        Token(TokenType.NUMBER, '6', 2, 29),
+        Token(TokenType.OP, '|', 2, 31),
+        Token(TokenType.NUMBER, '7', 2, 33),
+        Token(TokenType.OP, '<<', 2, 35),
+        Token(TokenType.NUMBER, '8', 2, 37),
+        Token(TokenType.OP, '>>', 2, 39),
+        Token(TokenType.NUMBER, '9', 2, 41),
+        Token(TokenType.EOF, '', 2, 42)
+    ]
+    assert tokens == expected_tokens
